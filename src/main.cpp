c9c9acf0c742e6f132e8499d37c9f07ff0f682b3
casefoster-wq <65779681+casefoster@users.noreply.github.com>
2026-09-05 14:23:23 -0400

    Updating branchVector2D

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "Vector2D.h"
 
 int main(){
-    std::cout << "GNC Missle Interceptor Simulation" << std::endl;
+    Vector2D v(3.0, 4.0);
+
+    std::cout << "X: " << v.getX() << std::endl;
+    std::cout << "Y: " << v.getY() << std::endl;
+    std::cout << "Magnitude: " << v.magnitude() << std::endl;
 
     return 0;
+
 }
